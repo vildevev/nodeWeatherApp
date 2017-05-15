ed5760aa246a5f9c87490c557de81eb01055b46d
“Vilde <vilde@vevatne.no>
2017-05-15 15:32:35 -0700

    Adds get weather function

--- geocode/geocode.js
+++ geocode/geocode.js
@@ -16,15 +16,36 @@ request({
     let results = body.results[0];
     let address = results.formatted_address;
     let latLng = results.geometry.location;
-    console.log(`the address is ${address}`)
-    console.log(`the latitude is ${latLng.lat}`)
-    console.log(`the longtitude is ${latLng.lng}`)
+    queryDarkSky(latLng);
+
 } else if (body.status === 'ZERO_RESULTS') {
-    console.log('I am sorry. No address match found')
+    console.log('I am sorry. No address match found');
+    return undefined
 } else {
-  console.log(body.error_message)
+  console.log(body.error_messages);
+  return undefined
 }
-})};
+})
+};
+
+var queryDarkSky = (latLng) => {
+  var baseURL = 'https://api.darksky.net/forecast/4d13194a64c26a3b07e2522d08daa476/';
+  var url = baseURL + encodeURIComponent(`${latLng.lat},${latLng.lng}`);
+  request({
+    url: url,
+    json: true
+  }, (error, response, body) => {
+    if (error) {
+      console.log("Could not connect to Dark Sky servers");
+      console.log(error);
+    } else {
+
+      console.log("Horray! We were able to connect to DarkSky servers");
+      console.log(body.currently.temperature);
+      console.log(body.daily.summary);
+    }
+  })
+}
 
 module.exports = {
   geocode
