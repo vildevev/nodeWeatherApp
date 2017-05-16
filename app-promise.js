e3b41616b52f0cf2a3a9d825af04cbc4f05e5b78
“Vilde <vilde@vevatne.no>
2017-05-15 18:54:56 -0700

    Adds weather announcements to app-promise file

--- app-promise.js
+++ app-promise.js
@@ -15,15 +15,20 @@ const args = require('yargs')
 
 const baseURL = 'https://maps.googleapis.com/maps/api/geocode/json?address='
 var url = baseURL + encodeURIComponent(args.address);
-let baseWeatherURL = `https://api.darksky.net/forecast/${process.env.DARK_SKY_API_KEY}/`;
-let weatherURL = baseWeatherURL + encodeURIComponent(`${latLng.lat}, ${latLng.lng}`)
 
 axios.get(url).then((response) => {
   if(response.data.status === 'ZERO_RESULTS') {
     throw new Error ('Unable to find that address');
   }
-  console.log(response.data);
-
+  let results = response.data.results[0];
+  let address = results.formatted_address;
+  let latitude = results.geometry.location.lat;
+  let longitude = results.geometry.location.lng;
+  let baseWeatherURL = `https://api.darksky.net/forecast/${process.env.DARK_SKY_API_KEY}/`;
+  let weatherURL = baseWeatherURL + encodeURIComponent(`${latitude}, ${longitude}`)
+  return axios.get(weatherURL)
+}).then((response) => {
+  console.log(`The temperature is ${response.data.currently.temperature}, it feels like ${response.data.currently.apparentTemperature}`)
 }).catch((e) => {
   if (e.code === 'ENOTFOUND'){
     console.log('Unable to connect to Google servers')
